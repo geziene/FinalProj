dc97d6f4dfbc47086e91e403b3f6eb2882f9badf
Nicklas Jensen <Nicklas Jensen@10.16.167.40>
2015-06-17 17:02:31 +0200

    DoubleExceptions

---
+++ src/dtu/shared/DoubleExceptions.java
@@ -0,0 +1,29 @@
+package dtu.shared;
+
+import com.google.gwt.user.client.Window;
+
+public class DoubleExceptions {
+	
+	
+	public boolean DoubleStringTest(String TestDoubleString) {
+		if (!TestDoubleString.contains("-"))
+		try {
+	        Double.parseDouble(TestDoubleString);
+	        return true;
+	    } catch (NumberFormatException e) {
+	    	Window.alert("Double maa kun indholde tal og punktum");
+	        return false;
+	    }
+		else{
+			Window.alert("Vaerdien maa ikke vaere negativ");
+	        return false;
+		}
+	}
+	
+	public double DoubleDecimalTest(String maengdeString) {
+		double maengde;
+		double scale = Math.pow(10, 4); 
+		maengde = Math.round(Double.parseDouble(maengdeString) * scale) / scale;
+		return maengde;
+	}
+}
